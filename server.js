6b7ebd0694904d8a21131e5690a6bc42751457f0
MatthewFinnegan1 <mjfinnegan@umass.edu>
2024-11-09 19:09:47 -0500

    Your commit message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const Database = require('./database');
 const MAX_PINS = 30;
 
